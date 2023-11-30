b695bc2e409a277fcda9f1f201c14dabf33ac1fd
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-11-30 13:29:54 -0500

    delete from other tables when block or allow

--- app/routes/add.js
+++ app/routes/add.js
@@ -4,45 +4,65 @@ const prisma = require("../prisma/client");
 const thresholding = require("../utils/threshold");
 
 router.post("/", async (req, res) => {
-    const stored = await thresholding.storeRequest(
-        req.body.url,
-        req.body,
-        req.ip
-    );
-    if (stored) {
-        return res.status(200).json({ msg: "added" });
-    } else {
-        return res.status(400).json({ msg: "Too soon" });
-    }
+  const stored = await thresholding.storeRequest(
+    req.body.url,
+    req.body,
+    req.ip
+  );
+  if (stored) {
+    return res.status(200).json({ msg: "added" });
+  } else {
+    return res.status(400).json({ msg: "Too soon" });
+  }
 });
 
 router.post("/allow", async (req, res) => {
-    const url = req.body.url;
-    const new_entry = await prisma.allowList.create({
-        data: {
-            url: url,
-        },
-    });
-    return res.status(200).json({ new_entry });
+  const url = req.body.url;
+  const new_entry = await prisma.allowList.create({
+    data: {
+      url: url,
+    },
+  });
+  await prisma.blockList.deleteMany({
+    where: {
+      url,
+    },
+  });
+  await prisma.request.deleteMany({
+    wherE: {
+      url,
+    },
+  });
+  return res.status(200).json({ new_entry });
 });
 
 router.post("/block", async (req, res) => {
-    const url = req.body.url;
-    const new_entry = await prisma.blockList.create({
-        data: {
-            url: url,
-        },
-    });
-    return res.status(200).json({ new_entry });
+  const url = req.body.url;
+  const new_entry = await prisma.blockList.create({
+    data: {
+      url: url,
+    },
+  });
+  await prisma.allowList.deleteMany({
+    where: {
+      url,
+    },
+  });
+  await prisma.request.deleteMany({
+    wherE: {
+      url,
+    },
+  });
+  return res.status(200).json({ new_entry });
 });
 
 router.post("/hasThreshold", async (req, res) => {
-    const result = await thresholding.runBFT(req.body.url);
-    if (result) {
-        return res.status(200).json({ msg: "passed" });
-    } else {
-        return res.status(400).json({ msg: "Failed" });
-    }
+  const result = await thresholding.runBFT(req.body.url);
+  if (result) {
+    return res.status(200).json({ msg: "passed" });
+  } else {
+    return res.status(400).json({ msg: "Failed" });
+  }
 });
 
 module.exports = router;
