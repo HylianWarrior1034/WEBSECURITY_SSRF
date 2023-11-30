310b5436ac2e2fa4b1afa84dedae24ae3d0260fa
arnavsshah <shah.arnav@yahoo.com>
2023-11-29 20:05:38 -0500

    [Remove] movies and news routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/", require("./home"));
-router.use("/news", require("./news"));
-router.use("/movies", require("./movies"));
 router.use("/add", require("./add"));
 router.use("/query", require("./query"));
 
